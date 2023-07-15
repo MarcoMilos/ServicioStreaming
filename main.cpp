e21b36e6a49579a2c955c4fb5747d4824c14a6fc
Marco Milos <marcomiloslavich03@gmail.com>
2023-07-14 22:07:01 -0600

    Add main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Marco & Alejandro Miloslavich Airola
+// 14-07-2023
+
+#include <iostream>
+#include <list>
+#include "video.h"
+#include "pelicula.h"
+#include "serie.h"
+
+int main()
+{
+    return 0;
+}
